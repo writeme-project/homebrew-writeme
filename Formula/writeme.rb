3d5087060247d7f65cb2317817614fb57c4871aa
BabelDev0 <leonardo.straccali@gmail.com>
2023-05-20 23:17:49 +0200

    refactor(formula): added dependencies files

--- Formula/writeme.rb
+++ Formula/writeme.rb
@@ -2,12 +2,13 @@ class Writeme < Formula
   desc "Cross-platform README generator for your project"
   homepage "https://github.com/BabelDev0/writeme"
   url "https://github.com/BabelDev0/writeme/releases/download/v0.1.0/writeme-mac.tar.gz"
-  sha256 "c3f2c72adc7fb0dc47b977c99d6d9f881fef78be2d7b6f78c8ed060788323c6f"
+  sha256 "6f7c4fb80c4cd8e47a8598af5be230e478fc86948cab30b0bd922e7d2773dc87"
   version "0.1.0"
 
   def install
-    prefix.install "configs.yml"
-    prefix.install "techs.yml"
+    prefix.install Dir["conf/*"];
+    prefix.install Dir["tpl/*"];
+
     bin.install "writeme"
   end
 end
