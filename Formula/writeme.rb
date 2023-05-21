552935742200f56c59a472b296bdb90fb8dc0733
BabelDev0 <leonardo.straccali@gmail.com>
2023-05-21 22:28:23 +0200

    refactor(formula): bump version

--- Formula/writeme.rb
+++ Formula/writeme.rb
@@ -1,14 +1,11 @@
 class Writeme < Formula
   desc "Cross-platform README generator for your project"
   homepage "https://github.com/BabelDev0/writeme"
-  url "https://github.com/BabelDev0/writeme/releases/download/v0.1.0/writeme-mac.tar.gz"
-  sha256 "e979b389872ecb942c25c60d2b309ef24b0786ee9b098f1b38a5d9c082f39496"
-  version "0.1.0"
+  url "https://github.com/BabelDev0/writeme/releases/download/v0.1.6/writeme-mac.tar.gz"
+  sha256 "584c5790ad65505a70bcf32e85a7fd0d20963593dfebffbfeac9df9d10537c2c"
+  version "0.1.6"
 
   def install
-    prefix.install Dir["conf/*"];
-    prefix.install Dir["tpl/*"];
-
     bin.install "writeme"
   end
 end
