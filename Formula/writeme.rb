45b2143e645701961130732dc2db63b8fa5c44b0
BabelDev0 <leonardo.straccali@gmail.com>
2023-05-29 14:27:57 +0200

    feat(formula): update with new repo under writeme-project organization

--- Formula/writeme.rb
+++ Formula/writeme.rb
@@ -1,9 +1,9 @@
 class Writeme < Formula
   desc "Cross-platform README generator for your project"
-  homepage "https://github.com/BabelDev0/writeme"
-  url "https://github.com/BabelDev0/writeme/releases/download/v0.1.6/writeme-mac.tar.gz"
-  sha256 "584c5790ad65505a70bcf32e85a7fd0d20963593dfebffbfeac9df9d10537c2c"
-  version "0.1.6"
+  homepage "https://github.com/writeme-project/writeme"
+  url "https://github.com/writeme-project/writeme/releases/download/v.0.1.7/writeme-mac.tar.gz"
+  sha256 "d273eaaa5f02ffccb629d7cfc27b39aee540e0a9ad2d23742872e95facf602b4"
+  version "0.1.7"
 
   def install
     bin.install "writeme"
