e555bb2a7b376a1d98cd3eab6cb626054908c955
BabelDev0 <leonardo.straccali@gmail.com>
2023-05-29 15:01:56 +0200

    feat(formula): minor update sha

--- Formula/writeme.rb
+++ Formula/writeme.rb
@@ -2,7 +2,7 @@ class Writeme < Formula
   desc "Cross-platform README generator for your project"
   homepage "https://github.com/writeme-project/writeme"
   url "https://github.com/writeme-project/writeme/releases/download/v.0.1.7/writeme-mac.tar.gz"
-  sha256 "d273eaaa5f02ffccb629d7cfc27b39aee540e0a9ad2d23742872e95facf602b4"
+  sha256 "64f0bdc2b98a3531e79eef93211a70a362a0c7c775663d063c070c7cdcea5007"
   version "0.1.7"
 
   def install
